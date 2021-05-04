2016673cc6ef738a1eab84b59fd8fba5b0ed9c38
ArroroMonk <83257946+ArroroMonk@users.noreply.github.com>
2021-05-04 14:18:12 -0400

    created file

---
+++ interfaces/iteratorInterfaceExample.java
@@ -0,0 +1,13 @@
+class iteratorsExample implements Iterator
+{ //the iterator interface is used to move through a list, rather then representing the list itself
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	public Object next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
